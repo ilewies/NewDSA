57c962bb292ede02e39030796a8a99304d43bd8f
Lavesh Satim <lavesh.git@gmail.com>
2024-04-09 06:08:45 +0530

    Update PrimeNumber.java

--- src/main/java/com/dsa/NewDSA/PrimeNumber.java
+++ src/main/java/com/dsa/NewDSA/PrimeNumber.java
@@ -2,23 +2,38 @@ package com.dsa.NewDSA;
 
 public class PrimeNumber {
 
-	public static String isPrime(int n) {
-
-		String no = "";
+	public static boolean isPrime(int n) {
 
 		for (int i = 2; i < n; i++) {
 			if (n % i == 0) {
-				return "not a prime";
+				return false;
 			}
 		}
 
-		return "prime no";
+		return true;
+	}
+
+	public static boolean isPrime2(int n) {
+		int factor = 0;
+
+		if (n == 1) {
+			return true;
+		}
+
+		for (int i = 1; i <= n; i++) {
+
+			if (n % i == 0) {
+				factor++;
+			}
+		}
+
+		return factor == 2;
 	}
 
 	public static void main(String... args) {
 
 		for (int i = 1; i <= 20; i++) {
-			System.out.println(i+" "+PrimeNumber.isPrime(i));
+			System.out.println(i + " " + PrimeNumber.isPrime(i) + " " + isPrime2(i));
 		}
 
 		;
