171bc97e5d91ef0dbc88054f4eb949907841e024
Lavesh Satim <lavesh.git@gmail.com>
2024-04-09 06:58:29 +0530

    ok

---
+++ src/main/java/com/dsa/Base/Addition.java
@@ -0,0 +1,18 @@
+package com.dsa.Base;
+
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("a is");
+		int a = scanner.nextInt();
+
+		System.out.println("b is ");
+		int b = scanner.nextInt();
+		System.out.println("i --> " + a+b);
+	}
+}
